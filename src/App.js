f07b838839dd5aca05de146da579496df474edec
rosscarnegie <97925198+rosscarnegie@users.noreply.github.com>
2022-02-17 20:03:38 -0500

    last save before I start moving things back. Use if I break

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import InfoSheet from './Components/InfoSheet/InfoSheet'
 import Footer from './Components/Footer/Footer'
 import './App.css';
 
-
-
 function App() {
   const startThumbnails = [
     {comName: "Bird1",
@@ -19,56 +17,53 @@ function App() {
     {comName: "Bird4",
     sciName: "Birdus Quatrus"},
     {comName: "Bird5",
-    sciName: "Birdus Cinqus"},
-  ]
+    sciName: "Birdus Cinqus"}]
   
-  const [currentThumbNails, setThumbNails] = useState(startThumbnails);
+const [currentThumbNails, setThumbNails] = useState(startThumbnails);
   
-  const [currentCoord, setCoord] = useState({
-    latitude: "",
-    longitude: "",
-  })
+const [currentCoord, setCoord] = useState({
+  latitude: "",
+  longitude: ""
+})
 
 const handleChange = (event) => {
   setCoord({...currentCoord, [event.target.name]: event.target.value})
-  //setThumbNails({...currentThumbNails, [event.target.name]: event.target.value})
 }
 
 const handleSubmit = (event) => {
   event.preventDefault();
 
-  const requestOptions = {
-    method: 'GET',
-    headers: {"X-eBirdApiToken": "3ehbfu8dqaf"},
-    redirect: 'follow'};
+const requestOptions = {
+  method: 'GET',
+  headers: {"X-eBirdApiToken": "3ehbfu8dqaf"},
+  redirect: 'follow'};
 
-  fetch(`https://api.ebird.org/v2/data/obs/geo/recent?lat=${currentCoord.latitude}&lng=${currentCoord.longitude}&sort=date&dist=50&back=30`, requestOptions)
-    .then(response => response.json())
-    .then(result => setThumbNails(result))
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
-    console.log(currentCoord);
-    setCoord({latitude: "", longitude: ""})
-    console.log(currentThumbNails)}
+fetch(`https://api.ebird.org/v2/data/obs/geo/recent?lat=${currentCoord.latitude}&lng=${currentCoord.longitude}&sort=date&dist=50&back=30`, requestOptions)
+  .then(response => response.json())
+  .then(result => setThumbNails(result))
+  .catch(error => console.log('error', error))
+}
 
-    const mappedThumbnails = currentThumbNails.map((singleThumbnail, index) => {
-      return(
-      <div key={index} className="thumbNail"><p>{singleThumbnail.comName}</p></div>
-    )})
-  
-  return (
-    <div className='appFrame'>
-      <header>
-        <Cover />
-        <nav><ul><li>Home</li></ul>
+const mappedThumbnails = currentThumbNails.map((singleThumbnail, index) => {
+  return(
+  <div key={index} className="thumbNail"><p>{singleThumbnail.comName}</p></div>
+  )
+})
+
+return (
+  <div className='appFrame'>
+    <header>
+      <Cover />
+      <nav><ul><li>Home</li></ul>
         <form className='coordForm' onSubmit={handleSubmit}>
-            <input type='text' name='latitude' placeholder='Latitude' value={currentCoord.latitude} onChange={handleChange}/>
-            <input type='text' name='longitude' placeholder='Longitude' value={currentCoord.longitude} onChange={handleChange}/>
-            <button>Find</button>
-        </form></nav>
+          <input type='text' name='latitude' placeholder='Latitude' value={currentCoord.latitude} onChange={handleChange}/>
+          <input type='text' name='longitude' placeholder='Longitude' value={currentCoord.longitude} onChange={handleChange}/>
+          <button>Find</button>
+        </form>
+      </nav>
 {/*   <NavBar/> */}  
-      </header>
-      <main className="thumbNailsList">
+    </header>
+    <main className="thumbNailsList">
       {mappedThumbnails ? mappedThumbnails : 'Loading...'}
         </main>
       
